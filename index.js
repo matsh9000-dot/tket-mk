341cb6d0dac976a1ec0f6e739ab4d688a11d5c75
matsh9000-dot <matsh9000@gmail.com>
2026-05-02 13:24:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -686,6 +686,4 @@ client.on("error", (err) => console.log(err));
 process.on("uncaughtException", (err) => console.log(err));
 process.on("uncaughtExceptionMonitor", (err) => console.log(err));
 process.on("rejectionHandled", (err) => console.log(err)); 
-const config = require('./config.json');
-
-client.login(config.token);
+client.login(process.env.TOKEN);
